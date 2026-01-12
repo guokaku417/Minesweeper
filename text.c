cc3c3576ae210a0fbbc6c7534d786a2481bbd68e
guokaku <2152228364@qq.com>
2026-01-12 17:58:30 +0800

    Upload Minesweeper game source code

---
+++ text.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+void menu()
+{
+	printf("******************\n");
+	printf("****  1.play  ****\n");
+	printf("****  0.exit  ****\n");
+	printf("******************\n");
+}
+
+void game()
+{
+	char mine[ROWS][COLS] = { 0 };//存放布置好的雷的信息
+	char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
+
+	//mine 数组初始化为'0'
+	InitBoard(mine, ROWS, COLS, '0');
+	//show 数组初始化为'*'
+	InitBoard(show, ROWS, COLS, '*');
+
+	//设置雷
+	SetMine(mine, ROW, COL);
+
+	//显示棋盘
+	DisplayBoard(show, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+
+	//排查雷
+	FindMine(mine, show, ROW, COL);
+
+}
+
+int main()
+{
+	int input = 0;
+
+	//设置随机数的生成起点
+	srand((unsigned int)time(NULL));
+
+	do
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
+	} while (input);
+	
+	return 0;
+}
